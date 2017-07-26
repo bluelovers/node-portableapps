889c50347bc61240cef07973fd2fca1828d29bb1
bluelovers <codelovers@users.sourceforge.net>
2017-07-27 05:41:15 +0800

    init (Step: 3)
    Step: 3
    
    [A ] new file:
    
    	index.js
    	package.json
    	src/build.js
    	src/config.js
    	src/filter.js
    	src/fs.js
    	src/portableapps/format.js
    	tpl/default/App/AppInfo/Launcher/AppNamePortable.ini
    	tpl/default/App/AppInfo/Launcher/Custom.nsh
    	tpl/default/App/AppInfo/Launcher/Splash.jpg
    	tpl/default/App/AppInfo/ShedkoFolderico4_136612429.ico
    	tpl/default/App/AppInfo/ShedkoFolderico4_156417750.ico
    	tpl/default/App/AppInfo/appicon.ico
    	tpl/default/App/AppInfo/appicon_128.png
    	tpl/default/App/AppInfo/appicon_16.png
    	tpl/default/App/AppInfo/appicon_256.png
    	tpl/default/App/AppInfo/appicon_32.png
    	tpl/default/App/AppInfo/appinfo.ini
    	tpl/default/App/AppInfo/installer.ini
    	tpl/default/App/DefaultData/.gitkeep
    	tpl/default/App/DefaultData/ShedkoFolderico4_66691170.ico
    	tpl/default/App/DefaultData/settings/.gitkeep
    	tpl/default/App/Portableapps.app.ico
    	tpl/default/App/Portableapps.data.ico
    	tpl/default/App/Readme.txt
    	tpl/default/App/ShedkoFolderico4_96211170.ico
    	tpl/default/App/bin/.gitkeep
    	tpl/default/Data/.gitkeep
    	tpl/default/Data/ShedkoFolderico4_99625065.ico
    	tpl/default/Other/Help/Images/Donation_Button.png
    	tpl/default/Other/Help/Images/Favicon.ico
    	tpl/default/Other/Help/Images/Help_Background_Footer.png
    	tpl/default/Other/Help/Images/Help_Background_Header.png
    	tpl/default/Other/Help/Images/Help_Logo_Top.png
    	tpl/default/Other/Source/AppNamePortable.ini
    	tpl/default/Other/Source/LauncherLicense.txt
    	tpl/default/Other/Source/License.txt
    	tpl/default/Other/Source/PortableApps.comLauncherCustom.nsh
    	tpl/default/Other/Source/PortableApps.comLauncherDebug.nsh
    	tpl/default/Other/Source/Readme.txt
    	tpl/default/ShedkoFolderico4_68017851.ico
    	tpl/default/help.html

---
+++ src/build.js
@@ -0,0 +1,192 @@
+/**
+ * Created by user on 2017/7/27/027.
+ */
+
+'use strict';
+
+const ejs = require('ejs');
+const Promise = require("bluebird");
+const { fs, winattr } = require('./fs');
+const globby = require('globby');
+const path = require('path');
+const filter = require('./filter');
+
+const upperCamelCase = require('uppercamelcase');
+
+module.exports.config = function (input = {}, extend = {})
+{
+	return require('./config').config(input, extend);
+};
+
+module.exports.options = function (options = {}, data = {})
+{
+	options = Object.assign({
+
+		tpl_name: 'default',
+
+		cwd: null,
+		cwd_base: path.join(__dirname, '..'),
+
+		source_dir: null,
+		target_dir: null,
+
+		target_dir_base: 'test/temp',
+		source_dir_base: 'tpl',
+
+	}, filter.object(options));
+
+	let out = upperCamelCase((data.app.name_id || options.tpl_name) + 'Portable');
+
+	if (!options.cwd)
+	{
+		options.cwd = process.cwd();
+	}
+
+	if (!options.source_dir)
+	{
+		options.source_dir = path.join(options.source_dir_base, options.tpl_name);
+	}
+
+	if (!fs.existsSync(options.source_dir))
+	{
+		options.source_dir = path.join(options.cwd_base, options.source_dir);
+	}
+
+	if (options.cwd.indexOf(options.cwd_base) != -1 || options.cwd.indexOf(path.join(__dirname, '..')) != -1)
+	{
+		options.cwd = options.cwd_base;
+	}
+
+	if (options.target_dir)
+	{
+		if (!path.isAbsolute(options.target_dir))
+		{
+			options.target_dir = path.join(options.cwd, options.target_dir);
+		}
+
+		if (options.target_dir.indexOf(options.cwd_base) != -1 || options.target_dir.indexOf(path.join(__dirname, '..')) != -1)
+		{
+			options.target_dir = null;
+		}
+
+		//console.log(options.target_dir);
+	}
+
+	if (!options.target_dir)
+	{
+		options.target_dir = path.join(options.target_dir_base);
+	}
+
+	if (!fs.existsSync(options.target_dir))
+	{
+		options.target_dir = path.join(options.cwd_base, options.target_dir);
+	}
+
+	options.target_dir = path.join(options.target_dir, out);
+
+	return options;
+};
+
+module.exports.build = async (options, data) =>
+{
+	console.time(data.pkg.name);
+
+	data = module.exports.config(options.app, data);
+	options = module.exports.options(options, data);
+
+	console.log(options);
+	console.log(data);
+
+	let source_dir = options.source_dir;
+	let target_dir = options.target_dir;
+
+	let source = path.join(source_dir);
+	let target = path.join(target_dir);
+
+	console.log([source, target, process.cwd()]);
+
+
+	console.log('start...');
+
+	return await globby(['**'], {
+		cwd: source,
+	})
+		.then(async (ls) =>
+		{
+			if (!ls.length)
+			{
+				console.error('error');
+				throw new Error();
+
+				return 1;
+			}
+
+			await fs.emptyDir(target);
+			console.warn(`[clear] ${target}`);
+
+			let fs_options = {
+				preserveTimestamps: true,
+			};
+
+			for (let filename of ls)
+			{
+				let file_src = path.join(source, filename);
+
+				let stat = await fs.stat(file_src);
+				let attr = await winattr.get(file_src);
+
+				let file = path.join(target, filename);
+
+				let ext = filename.split('.').pop();
+
+				await fs.copy(file_src, file, fs_options);
+
+				if (stat.isFile())
+				{
+					let basename = path.basename(filename);
+					let basename_new = `${data.app.name_id}Portable.ini`;
+
+					if (basename == 'AppNamePortable.ini' && basename != basename_new)
+					{
+						let file_new = path.join(target, path.dirname(filename), basename_new);
+						//console.log([basename, basename_new, file]);
+
+						await fs.move(file, file_new, fs_options);
+						console.info(`[rename] ${file} => ${file_new}`);
+
+						file = file_new;
+					}
+
+					if (['ini', 'html', 'htm', 'txt'].includes(ext))
+					{
+						let tpl = await fs.readFile(file_src);
+
+						let tpl_render = ejs.render(tpl.toString(), data, {});
+
+						if (tpl != tpl_render)
+						{
+							console.info(`[render] ${file}`);
+
+							//console.log(777, tpl)
+							//console.log(888, tpl_render);
+
+							await fs.writeFile(file, tpl_render);
+						}
+					}
+				}
+
+				await winattr.set(file, attr);
+			}
+
+			console.log(`end.`);
+			console.timeEnd(data.pkg.name);
+
+			return 0;
+		})
+		.catch((e) =>
+		{
+			console.error(e);
+			console.timeEnd(data.pkg.name);
+		})
+	;
+};
