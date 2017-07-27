ff889c4d899fce9d10ded3ec7c779fe363352216
bluelovers <codelovers@users.sourceforge.net>
2017-07-27 20:40:47 +0800

    https://portableapps.com/development/portableapps.com_format (Step: 1)
    Step: 1
    
    [ M] modified - Changes not staged for commit:
    
    	src/portableapps/format.js

--- src/portableapps/format.js
+++ src/portableapps/format.js
@@ -2,6 +2,11 @@
  * Created by user on 2017/7/26/026.
  */
 
+/**
+ * https://portableapps.com/development/portableapps.com_format
+ *
+ * @type {{type: string, version: number}}
+ */
 module.exports = {
 	//type: 'PortableApps.comFormat',
 	type: 'PortableAppsFormat',
